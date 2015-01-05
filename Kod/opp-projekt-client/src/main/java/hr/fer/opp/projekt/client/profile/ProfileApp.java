08d6d5a4c4ec00a93e89cbb69ce9fa18712b3d69
Paula <gombarica@gmail.com>
2015-01-05 01:53:58 +0100

    Profil korisnika u tijeku.

---
+++ Kod/opp-projekt-client/src/main/java/hr/fer/opp/projekt/client/profile/ProfileApp.java
@@ -0,0 +1,69 @@
+package hr.fer.opp.projekt.client.profile;
+
+import hr.fer.opp.projekt.client.communication.EventChannel;
+import hr.fer.opp.projekt.client.communication.OcsfEventChannel;
+import hr.fer.opp.projekt.common.model.Korisnik;
+
+import java.io.IOException;
+
+import com.lloseng.ocsf.client.ObservableClient;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class ProfileApp extends Application {
+
+	private Stage stage;
+	private AnchorPane root;
+	private EventChannel channel;
+	private ProfileController profileController;
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		final ObservableClient client = new ObservableClient("0.0.0.0", 5000);
+		client.openConnection();
+
+		this.channel = new OcsfEventChannel(client);
+
+		this.stage = stage;
+
+		this.stage.setTitle("Profile");
+		initRootLayout();
+	}
+
+	private void initRootLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(this.getClass().getClassLoader().getResource("fxml/profile/ProfileLayout.fxml"));
+			root = (AnchorPane) loader.load();
+			profileController = loader.getController();
+			profileController.setProfileApp(this);
+
+			Korisnik mirko = new Korisnik(4, "Mirko", "Mirkic", "kiki_bombon", "a", "hehe@a.com", "", "", "Bok!!!",
+					true, false);
+
+			profileController.setKorisnik(mirko);
+			
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public EventChannel getChannel() {
+		return channel;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
