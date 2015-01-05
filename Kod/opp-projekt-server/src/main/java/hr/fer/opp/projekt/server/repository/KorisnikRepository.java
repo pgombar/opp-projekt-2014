697911601b331abab8c1dabe913f33569790d3d3
Matija Folnovic <mfolnovic@gmail.com>
2015-01-05 23:32:23 +0100

    Pretraga umjetnika

--- Kod/opp-projekt-server/src/main/java/hr/fer/opp/projekt/server/repository/KorisnikRepository.java
+++ Kod/opp-projekt-server/src/main/java/hr/fer/opp/projekt/server/repository/KorisnikRepository.java
@@ -1,8 +1,15 @@
 package hr.fer.opp.projekt.server.repository;
 
 import hr.fer.opp.projekt.common.model.Korisnik;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface KorisnikRepository extends CrudRepository<Korisnik, Long> {
+    @Query("select u from Korisnik u where u.korisnickoIme like '%:korisnickoIme%' and " +
+            "u.ime like '%:ime%' and u.prezime like '%:prezime%'")
+    List<Korisnik> pretrazi(@Param("korisnickoIme") String korisnickoIme, @Param("ime") String ime,
+                            @Param("prezime") String prezime);
 }
