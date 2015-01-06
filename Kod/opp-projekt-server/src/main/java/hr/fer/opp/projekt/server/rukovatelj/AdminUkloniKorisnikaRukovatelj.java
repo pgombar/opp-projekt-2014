77cbb7283cc6c4af3b060c2c25aef0b34e439795
Matija Folnovic <mfolnovic@gmail.com>
2015-01-07 00:04:39 +0100

    Uklananje korisnika (admin).

--- Kod/opp-projekt-server/src/main/java/hr/fer/opp/projekt/server/rukovatelj/AdminUkloniKorisnikaRukovatelj.java
+++ Kod/opp-projekt-server/src/main/java/hr/fer/opp/projekt/server/rukovatelj/AdminUkloniKorisnikaRukovatelj.java
@@ -4,13 +4,29 @@ import com.lloseng.ocsf.server.ConnectionToClient;
 import hr.fer.opp.projekt.common.model.Korisnik;
 import hr.fer.opp.projekt.common.odgovor.AdminUkloniKorisnikaOdgovor;
 import hr.fer.opp.projekt.common.zahtjev.AdminUkloniKorisnikaZahtjev;
+import hr.fer.opp.projekt.server.repository.KorisnikRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public final class AdminUkloniKorisnikaRukovatelj implements RukovateljZahtjevom<AdminUkloniKorisnikaZahtjev, AdminUkloniKorisnikaOdgovor> {
+    private final KorisnikRepository korisnikRepository;
+
+    @Autowired
+    public AdminUkloniKorisnikaRukovatelj(KorisnikRepository korisnikRepository) {
+        this.korisnikRepository = korisnikRepository;
+    }
+
     @Override
     public AdminUkloniKorisnikaOdgovor handle(AdminUkloniKorisnikaZahtjev zahtjev, ConnectionToClient client,
                                               Korisnik active) {
-        return AdminUkloniKorisnikaOdgovor.USPJEH;
+        Korisnik korisnik = korisnikRepository.findOne(zahtjev.getId());
+
+        if (korisnik == null) {
+            return AdminUkloniKorisnikaOdgovor.NEUSPJEH;
+        } else {
+            korisnikRepository.delete(korisnik);
+            return AdminUkloniKorisnikaOdgovor.USPJEH;
+        }
     }
 }
