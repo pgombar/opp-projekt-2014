7a20537fbae5dfe774412e53e5cf13716a0f2437
Matija Folnovic <mfolnovic@gmail.com>
2015-01-06 13:42:05 +0100

    Prilagodba da ne inserta nove retke svaki put kad se aplikacija upali.

--- Kod/opp-projekt-server/src/main/resources/data.sql
+++ Kod/opp-projekt-server/src/main/resources/data.sql
@@ -1,14 +1,14 @@
-INSERT INTO GRANA (IME) VALUES
-  ('Grana 1'),
-  ('Grana 2');
+MERGE INTO GRANA (ID, IME) VALUES
+  (1, 'Grana 1'),
+  (2, 'Grana 2');
   
-INSERT INTO PODGRANA (IME) VALUES
-  ('Podgrana 1'),
-  ('Podgrana 2');
+MERGE INTO PODGRANA (ID, IME) VALUES
+  (1, 'Podgrana 1'),
+  (2, 'Podgrana 2');
   
-INSERT INTO KORISNIK(ADMIN, ADRESA, EMAIL, IME, KORISNICKO_IME, OSOBNI_STATUS, PREZIME, TELEFON, ZAPORKA, ZVANJE, GRANA_ID, PODGRANA_ID) VALUES 
-  (1, 'Ilica 10', 'mirko@gmail.com', 'Mirko', 'mmirkovic', 'Nisam zgodan al sam nezgodan', 'Mirkovic', '091472827', 'mirkec', 'soboslikar', 1, 1),
-  (1, 'Ilica 10', 'ankica@gmail.com', 'Ankica', 'aancic', 'Kaj buljis', 'Ancic', '091472827', 'ankica', 'soboslikar', 1, 1);
+MERGE INTO KORISNIK(ID, ADMIN, ADRESA, EMAIL, IME, KORISNICKO_IME, OSOBNI_STATUS, PREZIME, TELEFON, ZAPORKA, ZVANJE, GRANA_ID, PODGRANA_ID) VALUES
+  (1, 1, 'Ilica 10', 'mirko@gmail.com', 'Mirko', 'mmirkovic', 'Nisam zgodan al sam nezgodan', 'Mirkovic', '091472827', 'mirkec', 'soboslikar', 1, 1),
+  (2, 1, 'Ilica 10', 'ankica@gmail.com', 'Ankica', 'aancic', 'Kaj buljis', 'Ancic', '091472827', 'ankica', 'soboslikar', 1, 1);
 
 -- INSERT INTO PODGRANA (IME) VALUES ();
 
