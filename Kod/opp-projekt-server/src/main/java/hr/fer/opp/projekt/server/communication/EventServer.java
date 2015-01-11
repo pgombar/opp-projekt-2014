49ba577bb3d3a9de67dd1ca39c929d68e8b62ccc
Matija Folnovic <mfolnovic@gmail.com>
2015-01-11 18:57:03 +0100

    Dodano da se ispise na kojem hostu/portu je dignut server.

--- Kod/opp-projekt-server/src/main/java/hr/fer/opp/projekt/server/communication/EventServer.java
+++ Kod/opp-projekt-server/src/main/java/hr/fer/opp/projekt/server/communication/EventServer.java
@@ -24,140 +24,146 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class EventServer extends AbstractServer implements ApplicationContextAware {
-    private static final Logger LOGGER = LoggerFactory.getLogger(EventServer.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(EventServer.class);
 
-    private final int maxUsers;
+	private final String host;
 
-    private final KorisnikRepository korisnikRepository;
+	private final int port;
 
-    private final Map<Long, Connection> connections;
+	private final int maxUsers;
 
-    private final AtomicInteger userCount = new AtomicInteger();
+	private final KorisnikRepository korisnikRepository;
 
-    private ApplicationContext applicationContext;
+	private final Map<Long, Connection> connections;
 
-    @Autowired
-    public EventServer(@Value("${application.host}") String host, @Value("${application.port}") int port,
-                       @Value("${application.maxUsers}") int maxUsers, KorisnikRepository korisnikRepository) {
-        super(port);
+	private final AtomicInteger userCount = new AtomicInteger();
 
-        this.maxUsers = maxUsers;
-        this.korisnikRepository = korisnikRepository;
-        this.connections = new HashMap<>();
-    }
+	private ApplicationContext applicationContext;
+
+	@Autowired
+	public EventServer(@Value("${application.host}") String host, @Value("${application.port}") int port,
+					   @Value("${application.maxUsers}") int maxUsers, KorisnikRepository korisnikRepository) {
+		super(port);
+
+		this.host = host;
+		this.port = port;
+		this.maxUsers = maxUsers;
+		this.korisnikRepository = korisnikRepository;
+		this.connections = new HashMap<>();
+	}
 
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
-    }
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
 
-    @Override
-    protected void clientConnected(ConnectionToClient client) {
-        LOGGER.info("Client {} connected!", client.getInetAddress());
+	@Override
+	protected void clientConnected(ConnectionToClient client) {
+		LOGGER.info("Client {} connected!", client.getInetAddress());
 
-        connections.put(client.getId(), new Connection(client));
+		connections.put(client.getId(), new Connection(client));
 
-        super.clientConnected(client);
-    }
+		super.clientConnected(client);
+	}
 
-    @Override
-    protected synchronized void clientDisconnected(ConnectionToClient client) {
-        LOGGER.info("Client {} disconnected!", client.getInetAddress());
+	@Override
+	protected synchronized void clientDisconnected(ConnectionToClient client) {
+		LOGGER.info("Client {} disconnected!", client.getInetAddress());
 
-        removeKorisnik(client);
+		removeKorisnik(client);
 
-        super.clientDisconnected(client);
-    }
+		super.clientDisconnected(client);
+	}
 
-    @Override
-    protected void serverStarted() {
-        LOGGER.info("Server started");
+	@Override
+	protected void serverStarted() {
+		LOGGER.info("Server started at {}:{}", host, port);
 
-        super.serverStarted();
-    }
+		super.serverStarted();
+	}
 
-    @Override
-    protected void serverStopped() {
-        LOGGER.info("Server stopped");
+	@Override
+	protected void serverStopped() {
+		LOGGER.info("Server stopped");
 
-        super.serverStopped();
-    }
+		super.serverStopped();
+	}
 
-    @Override
-    protected void serverClosed() {
-        LOGGER.info("Server closed");
+	@Override
+	protected void serverClosed() {
+		LOGGER.info("Server closed");
 
-        super.serverClosed();
-    }
+		super.serverClosed();
+	}
 
-    @Override
-    @Transactional
-    protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
-        Zahtjev zahtjev = (Zahtjev) msg;
+	@Override
+	@Transactional
+	protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+		Zahtjev zahtjev = (Zahtjev) msg;
 
-        LOGGER.debug("Dosao zahtjev {}.", zahtjev);
+		LOGGER.debug("Dosao zahtjev {}.", zahtjev);
 
-        RukovateljZahtjevom<Zahtjev, ?> rukovatelj = findRukovatelj(zahtjev);
+		RukovateljZahtjevom<Zahtjev, ?> rukovatelj = findRukovatelj(zahtjev);
 
-        LOGGER.debug("Rjesavam zahtjev {} s {}", zahtjev, rukovatelj);
+		LOGGER.debug("Rjesavam zahtjev {} s {}", zahtjev, rukovatelj);
 
-        Connection connection = connections.get(client.getId());
+		Connection connection = connections.get(client.getId());
 
-        Odgovor odgovor = rukovatelj.handle(zahtjev, client, connection.korisnik);
+		Odgovor odgovor = rukovatelj.handle(zahtjev, client, connection.korisnik);
 
-        if (connection.korisnik != null) {
-            korisnikRepository.setZadnjiPutAktivanFor(connection.korisnik.getId(), new Date());
-        }
+		if (connection.korisnik != null) {
+			korisnikRepository.setZadnjiPutAktivanFor(connection.korisnik.getId(), new Date());
+		}
 
-        try {
-            LOGGER.debug("Odgovaram na zahtjev {} s {}.", zahtjev, odgovor);
+		try {
+			LOGGER.debug("Odgovaram na zahtjev {} s {}.", zahtjev, odgovor);
 
-            client.sendToClient(odgovor);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+			client.sendToClient(odgovor);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public boolean setKorisnik(ConnectionToClient client, Korisnik korisnik) {
-        if (userCount.incrementAndGet() > maxUsers) {
-            userCount.decrementAndGet();
-            return false;
-        } else {
-            connections.get(client.getId()).korisnik = korisnik;
-            return true;
-        }
-    }
+	public boolean setKorisnik(ConnectionToClient client, Korisnik korisnik) {
+		if (userCount.incrementAndGet() > maxUsers) {
+			userCount.decrementAndGet();
+			return false;
+		} else {
+			connections.get(client.getId()).korisnik = korisnik;
+			return true;
+		}
+	}
 
-    public void removeKorisnik(ConnectionToClient client) {
-        Korisnik korisnik = connections.get(client.getId()).korisnik;
-        korisnikRepository.setOnlineToFalseFor(korisnik.getId());
+	public void removeKorisnik(ConnectionToClient client) {
+		Korisnik korisnik = connections.get(client.getId()).korisnik;
+		korisnikRepository.setOnlineToFalseFor(korisnik.getId());
 
-        userCount.decrementAndGet();
-        connections.get(client.getId()).korisnik = null;
-    }
+		userCount.decrementAndGet();
+		connections.get(client.getId()).korisnik = null;
+	}
 
-    private RukovateljZahtjevom<Zahtjev, ?> findRukovatelj(Zahtjev zahtjev) {
-        for (RukovateljZahtjevom rukovatelj : applicationContext.getBeansOfType(RukovateljZahtjevom.class).values()) {
-            ParameterizedType genericSuperclass = (ParameterizedType) rukovatelj.getClass().getGenericInterfaces()[0];
-            Class<?> clazz = (Class<?>) genericSuperclass.getActualTypeArguments()[0];
+	private RukovateljZahtjevom<Zahtjev, ?> findRukovatelj(Zahtjev zahtjev) {
+		for (RukovateljZahtjevom rukovatelj : applicationContext.getBeansOfType(RukovateljZahtjevom.class).values()) {
+			ParameterizedType genericSuperclass = (ParameterizedType) rukovatelj.getClass().getGenericInterfaces()[0];
+			Class<?> clazz = (Class<?>) genericSuperclass.getActualTypeArguments()[0];
 
-            if (clazz == zahtjev.getClass()) {
-                return (RukovateljZahtjevom<Zahtjev, ?>) rukovatelj;
-            }
-        }
+			if (clazz == zahtjev.getClass()) {
+				return (RukovateljZahtjevom<Zahtjev, ?>) rukovatelj;
+			}
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    private static class Connection {
-        private final ConnectionToClient connection;
+	private static class Connection {
+		private final ConnectionToClient connection;
 
-        private Korisnik korisnik = null;
+		private Korisnik korisnik = null;
 
-        private Connection(ConnectionToClient connection) {
-            this.connection = connection;
-        }
-    }
+		private Connection(ConnectionToClient connection) {
+			this.connection = connection;
+		}
+	}
 
 }
